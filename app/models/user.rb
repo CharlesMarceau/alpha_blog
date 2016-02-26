81b7fb73046050256b89c077b9355966904a43df
Charles Marceau <charles.marceau7@gmail.com>
2016-02-26 12:40:40 -0500

    add association between user and article, and add debbuger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 } # active_record_validations.html
+  has_many :articles
+  before_save { self.email = email.downcase } #ajuster toutes les lettres en miniscule avant de saver
+  validates :username, presence: true,
+                       uniqueness: true,
+                       length: { minimum: 3, maximum: 25 } # active_record_validations.html
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i # regular expression
-  validates :email, presence: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, 
+                    uniqueness: true,
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
