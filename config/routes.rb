6737fa58f80bb4da8b5fa5b97a534b4b60b5a773
Charles Marceau <charles.marceau7@gmail.com>
2016-02-26 16:02:46 -0500

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   resources :articles # create editing options
 
+  get 'signup', to: "users#new"
+  # post 'users', to: "users#create"
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
