b5e34a24c40ae94b2135534a20a4732463822ec5
Charles Marceau <charles.marceau7@gmail.com>
2016-02-26 01:24:49 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 } # active_record_validations.html
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i # regular expression
+  validates :email, presence: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
